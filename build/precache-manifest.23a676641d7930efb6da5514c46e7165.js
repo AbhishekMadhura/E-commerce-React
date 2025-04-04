e1834985cb9a6eef874b07edb67acb5c304a9150
abhishek <abhishekmadhura34@gmail.com>
2025-04-04 21:29:50 +0530

    second

---
+++ build/precache-manifest.23a676641d7930efb6da5514c46e7165.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11971ea2d7f69bbf43b768e7ab61d2cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f86099edd8c93b1bad29",
+    "url": "/static/css/2.a10018f7.chunk.css"
+  },
+  {
+    "revision": "41ad9444252c790052f0",
+    "url": "/static/css/main.06b1c665.chunk.css"
+  },
+  {
+    "revision": "f86099edd8c93b1bad29",
+    "url": "/static/js/2.3e193b5e.chunk.js"
+  },
+  {
+    "revision": "380187970ab6f80f8ddcc04c2a41abf5",
+    "url": "/static/js/2.3e193b5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41ad9444252c790052f0",
+    "url": "/static/js/main.fcfc3625.chunk.js"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  },
+  {
+    "revision": "29467d4f3bf6a7d96f02cf4c32ce80ac",
+    "url": "/static/media/hero_image.29467d4f.jpg"
+  }
+]);
